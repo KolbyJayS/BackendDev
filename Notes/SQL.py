e55456dcd5b2637a6c9d1876d5b467c55cb42e76
Kolby Skolrood <kolbyjskolrood@gmail.com>
2026-02-16 17:09:30 -0800

    Final commit for beginner round of notes

--- Notes/SQL.py
+++ Notes/SQL.py
@@ -35,15 +35,40 @@ CRUD (Create, Read, Update, Delete)
 
 Indexes
     An index in SQL is a database structure used to speed up data retrieval by allowing quick access to records 
-        instead of scanning the full database
-    There are 3 main ways to create an index in SQL
-        Single Column Indexes : 
-
+        instead of scanning the full database. Think something like binary search.
+    This works by storing indexed column values in a seperate, ordered structure. Typically as a B-tree.
+        The DB then searches the index, finds row locations, and then retreives the rows.
+    Common Index Types:
+        Primary Key Index : created automatically
+        Unique Index : enforces uniqueness
+        Single Column Index : Single column index
+        Composite Index : Multiple column indexes
+    Benefits of an index are faster reads, joins, and sorting. However, every time the tables are updated the index need to
+        be updated as well resulting in slower insert, update, and delete functionality.
+        
 Constraints
-
+    PRIMARY KEY : uniquely identifies each row; cannot be NULL
+    FOREIGN KEY : enforces relationship between tables
+    UNIQUE : prevents duplicate values
+    NOT NULL : column must contain a value
+    CHECK : value must satisfy a condition
+    DEFAULT : assigns a value of none provided
 
 Basic Normalization
+    Process of structuring tables to reduce redundancy and prevent data anomalies
 
+    1st Normal Form (1NF)
+        No Repeating groups or arrays
+        Each field holds a single value
+        Each row identifiable by a key
+
+    2nd Normal Form (2NF)
+        Must be in 1NF
+        Non-key columns depend on the entire primary key (import for composite keys)
+
+    3rd Normal Form (3NF)
+        Must be in 2NF
+        Non-key columns depend only on the primary key, no other non-key columns
 
 """
 
